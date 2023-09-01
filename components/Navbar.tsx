2e6ee945cee11df79ee3af5883075ade7da5451b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-01 21:51:01 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,54 +3,54 @@
 import { motion } from "framer-motion";
 import { NavLinks } from "../constants";
 import { navVariants } from "../utils/motion";
+import navbarStyle from "../styles/Navbar.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [navActive, setNavActive] = useState(false);   
+  const [navActive, setNavActive] = useState(false);
 
   function toggle() {
     setNavActive(!navActive);
   }
   const pathname = usePathname();
   return (
-    <motion.nav
+    <motion.header
       variants={navVariants}
       initial="hidden"
       whileInView="show"
-      className="xPaddings py-4 fixed top-0 w-full bg-[#43a5f6]"
+      className={`${navbarStyle.header} ${navActive ? navbarStyle.active : ""}`}
     >
-      <div className="innerWidth mx-auto flex justify-between gap-8 items-center">
-        <img
-          src="/assets/logo1.jpg"
-          alt="search"
-          className="w-[40px] h-[40px] rounded-full object-contain"
-        />
-        <div className="menutoggle" onClick={toggle}></div>   
-        <div>
-          <ul className="hidden xl:flex text-small gap-7">
-            {NavLinks.map((link) => {
-              const isActive =
-                (pathname.includes(link.href) && link.href.length > 1) ||
-                pathname === link.href;
-              return (
+      <img
+        src="/assets/logo1.jpg"
+        alt="search"
+        className="w-[40px] h-[40px] rounded-full object-contain"
+      />
+      <div className={navbarStyle.menuToggle} onClick={toggle}></div>
+      <nav>
+        <ul>
+          {NavLinks.map((link) => {
+            const isActive =
+              (pathname.includes(link.href) && link.href.length > 1) ||
+              pathname === link.href;
+            return (
+              <li>
                 <Link
                   href={link.href}
                   key={link.key}
-                  className={`text-white transition-all 
-                  rounded-[18px] hover:bg-white hover:text-[#43a5f6] p-2 ${
+                  className={`hover:bg-white hover:text-[#43a5f6] p-2 ${
                     isActive ? "bg-white text-[#43a5f6]" : ""
                   }`}
                 >
                   {link.text}
                 </Link>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-    </motion.nav>
+              </li>
+            );
+          })}  
+        </ul>
+      </nav>
+    </motion.header>
   );
 };
 
