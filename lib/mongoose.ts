87ae7147b2dda29abb5ff77392972d17b11e6e76
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-01 11:21:04 +0100

    uploadthing

---
+++ lib/mongoose.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+export const connectToDB = async () => {  
+  mongoose.set("strictQuery", true, );
+  if (!process.env.MONGO_URL) return console.log("MONGO_URL not found");   
+  if (isConnected) return console.log("Already connected to MongoDB");
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    isConnected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
