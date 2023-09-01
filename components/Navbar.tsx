05df2b74f83cf2ddf9e90772aafa85c6a04516cc
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-01 18:13:06 +0100

    styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,30 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+"use client";
 
-export default Navbar
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-4 fixed top-0 w-full bg-[#43a5f6]`}
+  >
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/logo1.jpg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <div>
+        <h2 className="text-white font-extrabold text-[24px] leading-[30px]">
+          METAVERSUS
+        </h2>
+      </div>
+    </div>
+  </motion.nav>
+);
+
+export default Navbar;
