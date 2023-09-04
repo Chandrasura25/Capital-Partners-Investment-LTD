239cdacb3dd6c9939f0937deb768243220b06071
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-04 02:33:35 +0100

    pillars and cards

--- components/Pillar.tsx
+++ components/Pillar.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import { motion } from "framer-motion";
-import { staggerContainer, fadeIn } from "../utils/motion";
+import { staggerContainer, fadeIn, textVariant2 } from "../utils/motion";
 import pillarStyles from "../styles/Pillar.module.css";
 import Card from "./Card";
 import { pillarText } from "../constants";
@@ -12,17 +12,27 @@ const Pillar = () => {
   return (
     <>
       <section className="paddings relative z-10" id="pillars">
-      <div className="gradient-02 z-0" />
+        <div className="gradient-02 z-0" />
         <motion.div
-        variants={staggerContainer()}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className="innerWidth mx-auto flexCenter flex-col"
+          variants={staggerContainer()}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.25 }}
+          className="innerWidth mx-auto flexCenter flex-col"
         >
-          <TitleText title={<>| OUR CORE <span className="text-[#43a5f6]">INVESTMENT SECTORS</span> </> } textStyles="text-center" />
-          <motion.p variants={fadeIn("up", "tween", 0.2, 1)}
-          className="relative font-normal text-center px-[100px] text-white">
+          <TitleText
+            title={
+              <>
+                | OUR CORE{" "}
+                <span className="text-[#43a5f6]">INVESTMENT SECTORS</span>{" "}
+              </>
+            }
+            textStyles="text-center"
+          />
+          <motion.p
+            variants={fadeIn("up", "tween", 0.2, 1)}
+            className="relative font-normal text-center md:px-[100px] px-[20px] text-white"
+          >
             Our investment portfolio encompasses a wide range of sectors,
             providing opportunities for both domestic and international
             investors. We believe in the power of strategic investments to drive
@@ -30,21 +40,20 @@ const Pillar = () => {
             we have carefully selected sectors that are essential to Nigeria`s
             growth and have significant potential for long-term profitability.
           </motion.p>
-        <motion.div>
-          <motion.p className={pillarStyles.subText}>
-            THE <span>7 PILLARS</span> OF OUR INVESTMENT
-          </motion.p>
-          <motion.div className={pillarStyles.container}>
-            {pillarText.map((pillar, index) => (
-              <Card key={index} {...pillar} index={index} />
-            ))}
-          </motion.div>
-        </motion.div>
-        <motion.div
-          variants={fadeIn("up", "tween", 0.3, 1)}
-          className={pillarStyles.bottom}
-        >
-          <motion.p className="text-white font-normal">
+          <div>
+            <motion.p className={pillarStyles.subText} variants={textVariant2}>
+              THE <span>7 PILLARS</span> OF OUR INVESTMENT
+            </motion.p>
+            <div className={pillarStyles.container}>
+              {pillarText.map((pillar, index) => (
+                <Card key={index} {...pillar} index={index} />
+              ))}
+            </div>
+          </div>
+          <motion.p
+            className="relative font-normal text-center md:px-[100px] px-[20px] text-white"
+            variants={fadeIn("up", "tween", 0.3, 1)}
+          >
             At Capital Partners Investment LTD, we combine our extensive
             industry knowledge, market insights, and investment expertise to
             identify and execute value-driven opportunities. Our team of
@@ -55,7 +64,6 @@ const Pillar = () => {
             financial goals meet our strategic vision, creating a path to mutual
             success.
           </motion.p>
-        </motion.div>
         </motion.div>
       </section>
     </>
