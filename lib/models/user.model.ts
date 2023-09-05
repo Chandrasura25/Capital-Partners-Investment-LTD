6673630723994181f7109e054cc4011577e1d463
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-05 17:10:37 +0100

    user

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -2,10 +2,18 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     id: {type: String, required: true},
     username: {type: String, required: true, unique: true},
-    name: {type: String, required: true},
-    bio: String,
+    email: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    surname: String,
+    gender: String,
+    mobile_number: String,
+    next_of_kin: String,
+    mother_middle_name: String,
+    homeAddress: String,
+    officeAddress: String,
+    level_of_education: String,
     image:  String,
-    threads: [{type: mongoose.Schema.Types.ObjectId, ref: "Thread"}],
+    date_of_birth: String,
     onboarded: {type: Boolean, default: false},
 });
 const User = mongoose.models.User || mongoose.model("User", userSchema); 
