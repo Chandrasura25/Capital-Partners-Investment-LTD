8f86eb17a750a67978fbd4e6d5592af52db81f41
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-06 11:15:46 +0100

    nprogress

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,34 +1,33 @@
-import Footer from '@/components/Footer'
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import { Toaster } from "@/components/ui/toaster"
+import Footer from "@/components/Footer";
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Capital Partners Investment LTD",
   description: "Capital Partners Investment LTD",
-}
+
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <main className="bg-[#000924] overflow-hidden">
-        <Navbar />
-        <div>
-          {children}
-        </div>
-        <Footer />
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
         </main>
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
-  )
+  );
 }
