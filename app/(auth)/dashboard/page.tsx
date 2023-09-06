6936bb41e480fb90700c92935223b89e25341737
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-06 16:23:27 +0100

    dashboard

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import Bottombar from "@/components/shared/Bottombar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import Topbar from "@/components/shared/Topbar"
 
-const Page = () => {
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
+
   return (
-    <div>Page</div>
+    <>
+      <Topbar userInfo={userInfo} />
+
+      <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+
+              </div>
+            </section>
+          </main>
+
+          <Bottombar />
+    </>
   )
 }
 
