731d2819b08fe7e482d82c8c01386c0e5e7dcb9b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-06 12:50:23 +0100

    nprogress out

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,22 +1,3 @@
-// import mongoose from "mongoose"; 
-
-// let isConnected = false;
-
-// export const connectToDB = async () => {
-//   mongoose.set("strictQuery", true);
-
-//   if (!process.env.MONGO_URL) return console.log("MONGO_URL not found");
-//   if (isConnected) return console.log("Already connected to MongoDB");
-
-//   try {
-//     const mongoURLWithDBName = `${process.env.MONGO_URL}/${process.env.DB_NAME}`;
-//     await mongoose.connect(mongoURLWithDBName);
-//     isConnected = true;
-//     console.log(`Connected to MongoDB database: ${process.env.DB_NAME}`);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
 import mongoose from "mongoose";
 
 let isConnected = false;
