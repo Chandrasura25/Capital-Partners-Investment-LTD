517a87ea5b5d50160ac3c4a030b42018d1bb1948
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-06 16:32:36 +0100

    topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,14 +2,17 @@ import { SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 interface userProp{
-  
+  userInfo: {
+    username: string;
+    image: string;
+  };
 }
 function Topbar({userInfo}:userProp) {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
         <Image  src="/assets/logo1.jpg" alt="logo" width={30} height={30} className="rounded-full" />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">Capital Partners Investment LTD</p>
+        <p className="text-heading3-bold text-light-1 md:text-[14px] max-xs:hidden">Capital Partners Investment LTD</p>
       </Link>
 
       <div className="flex items-center gap-1">
@@ -30,11 +33,12 @@ function Topbar({userInfo}:userProp) {
          <div className="hidden md:block">
               <div className="flex cursor-pointer">
                 <Image
-                  src="/assets/logout.svg"
-                  alt="logout"
+                  src={userInfo.image}
+                  alt="User Logo"
                   width={24}
                   height={24}
                 />
+                <p className="text-[14px] text-heading3-bold text-light-3">{userInfo.username}</p>
               </div>
         </div>
       </div>
