b9aa3ee8ef13c311f92604ab599e4f60c92d5336
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-06 12:47:51 +0100

    mongoose

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,19 +1,34 @@
-import mongoose from "mongoose"; 
+// import mongoose from "mongoose"; 
+
+// let isConnected = false;
+
+// export const connectToDB = async () => {
+//   mongoose.set("strictQuery", true);
+
+//   if (!process.env.MONGO_URL) return console.log("MONGO_URL not found");
+//   if (isConnected) return console.log("Already connected to MongoDB");
+
+//   try {
+//     const mongoURLWithDBName = `${process.env.MONGO_URL}/${process.env.DB_NAME}`;
+//     await mongoose.connect(mongoURLWithDBName);
+//     isConnected = true;
+//     console.log(`Connected to MongoDB database: ${process.env.DB_NAME}`);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+import mongoose from "mongoose";
 
 let isConnected = false;
-
-export const connectToDB = async () => {
-  mongoose.set("strictQuery", true);
-
-  if (!process.env.MONGO_URL) return console.log("MONGO_URL not found");
+export const connectToDB = async () => {  
+  mongoose.set("strictQuery", true, );
+  if (!process.env.MONGO_URL) return console.log("MONGO_URL not found");   
   if (isConnected) return console.log("Already connected to MongoDB");
 
   try {
-    const mongoURLWithDBName = `${process.env.MONGO_URL}/${process.env.DB_NAME}`;
-    await mongoose.connect(mongoURLWithDBName);
+    await mongoose.connect(process.env.MONGO_URL);
     isConnected = true;
-    console.log(`Connected to MongoDB database: ${process.env.DB_NAME}`);
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
