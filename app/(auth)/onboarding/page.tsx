ded740eecab828e7a364cf7f2a42552f733226e6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-07 14:22:32 +0100

    dashboard

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -8,7 +8,7 @@ async function Page() {
   const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
   const userInfo = await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect("/");
+  if (userInfo?.onboarded) redirect("/dashboard");
 
   const userData = {
     id: user.id,
