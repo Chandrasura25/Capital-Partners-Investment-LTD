2cf83bf0dc015ec4680e5e2defb4de15c982a7e4
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-08 14:59:45 +0100

    profile

---
+++ app/(auth)/profile/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+async function Page({ params }: { params: { id: string } }) {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  return <div>Page</div>;
+}
+
+export default Page;
