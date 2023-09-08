1e1d8ef643c2ac17efe558cd1ba2a59c0f94da87
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-08 16:18:50 +0100

    profile

--- app/(auth)/profile/[id]/page.tsx
+++ app/(auth)/profile/[id]/page.tsx
@@ -2,7 +2,9 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { fetchUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
@@ -10,7 +12,20 @@ async function Page({ params }: { params: { id: string } }) {
 
   const userInfo = await fetchUser(params.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
-  return <div>Page</div>;
+  return (
+    <>
+      <Topbar userInfo={userInfo} />
+      <main className="flex flex-row bg-[#000924]">
+        <LeftSidebar />
+        <section className="main-container gradient-07">
+          <div className="w-full max-w-4xl">
+            <p className="text-xl text-light-1">Profile</p>
+          </div>
+        </section>
+      </main>
+      <Bottombar />
+    </>
+  );
 }
 
 export default Page;
