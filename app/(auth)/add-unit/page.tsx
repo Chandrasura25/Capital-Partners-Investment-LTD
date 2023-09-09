01605615d84d69ed2f556257d6c8b5d3c36ba651
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-09 19:39:24 +0100

    hover

--- app/(auth)/add-unit/page.tsx
+++ app/(auth)/add-unit/page.tsx
@@ -27,7 +27,7 @@ async function Page() {
               ))}
             </div>
             <div className="flex items-end justify-end">
-              <Button type="submit" className="bg-[#150B62] uppercase p-5 hover:border hover:border-[#150B62]">
+              <Button type="submit" className="bg-[#150B62] uppercase p-5 transition hover:bg-white hover:text-[#150B62]">
                 Invest Here
               </Button>
             </div>
