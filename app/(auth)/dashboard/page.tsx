85cb30f2febf2a9718a2a2381332144618217005
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-09 07:18:40 +0100

    bg

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -22,7 +22,7 @@ async function Page() {
         <LeftSidebar />
         <section className="main-container relative bg-[rgba(31, 38, 135, 0.37)]">
           <div className="w-full max-w-4xl">
-            <div className="relative flex flex-wrap gap-4 md:gap-6">
+            <div className="glassmorphism p-5 relative flex flex-wrap gap-4 md:gap-6">
               {dashboardMenu.map((link) =>  (
                   <Link
                     href={link.route}
