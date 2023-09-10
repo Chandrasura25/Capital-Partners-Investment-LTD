7a0fc0383508d81872e623ac9fe9a1ed774e6ed9
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-10 13:34:58 +0100

    slidein

--- app/(root)/product01/page.tsx
+++ app/(root)/product01/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { planetVariants, staggerContainer } from "@/utils/motion";
+import { slideIn, staggerContainer } from "@/utils/motion";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -13,8 +13,19 @@ const Page = () => {
         viewport={{ once: false, amount: 0.25 }}
         className="innerWidth mx-auto flexCenter flex-col min-h-[50vh]"
       >
-        <motion.div variants={planetVariants("left")}>
-            <Image src="/assets/logo1.jpg" height={200} width={200} alt="Logo" className="rounded-full" />
+        <motion.div variants={slideIn("left")}>
+          <Image
+            src="/assets/logo1.jpg"
+            height={200}
+            width={200}
+            alt="Logo"
+            className="rounded-full"
+          />
+        </motion.div>
+        <motion.div variants={slideIn("right")} className="flex flex-col">
+          <h1 className="text-3xl font-bold text-center text-white uppercase">
+            Invest with the best
+          </h1>
         </motion.div>
       </motion.div>
     </section>
