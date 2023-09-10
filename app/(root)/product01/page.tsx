2ae8f7390758dcb1a6f659b55157ffbf8fbb2ad4
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-10 13:15:07 +0100

    go

--- app/(root)/product01/page.tsx
+++ app/(root)/product01/page.tsx
@@ -1,21 +1,21 @@
 "use client";
-import { staggerContainer } from '@/utils/motion'
-import {motion} from 'framer-motion'
+import { staggerContainer } from "@/utils/motion";
+import { motion } from "framer-motion";
 
 const Page = () => {
   return (
-    <section className="paddings relative z-10 pt-[60px]">
+    <section className="paddings relative z-10 mt-[60px]">
       <motion.div
         variants={staggerContainer()}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className="innerWidth mx-auto flexCenter flex-col"
+        className="innerWidth mx-auto flexCenter flex-col min-h-screen"
       >
-       <h4 className="text-3xl">Welcome</h4> 
+        <h4 className="text-3xl">Welcome</h4>
       </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
