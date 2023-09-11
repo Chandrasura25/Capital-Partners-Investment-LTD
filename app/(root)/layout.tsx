59848e7b8e6e993b3cbd2f49ac2e411f4833b963
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-11 18:48:29 +0100

    description

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Capital Partners Investment LTD",
-  description: "Capital Partners Investment LTD",
+  description: "A leading investment company with a diverse portfolio covering various sectors of the economy.",
   manifest: "/manifest.json",
   icons: { apple: "/assets/logo1.jpg", android: "/assets/logo1.jpg" },
   themeColor: "#ffffff",
