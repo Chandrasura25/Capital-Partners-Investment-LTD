b28911bd9cf8218d745b57399d5090f1be3a6788
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-15 17:14:40 +0100

    bug

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,14 +4,15 @@ import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
   const user = await currentUser();
-  const userInfo = await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect("/dashboard");
   if (!user) {
     return (
       <div className="formBg flex w-full justify-center items-center min-h-screen">
         <SignIn />
       </div>
     );
+  }else{
+      const userInfo = await fetchUser(user.id);
+      if (userInfo?.onboarded) redirect("/dashboard");
   }
 }
 export default Page;
