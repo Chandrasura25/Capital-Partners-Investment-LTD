b79ea9205f9e74dc66be3b07bd2e4cb9188d5821
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-15 17:03:59 +0100

    testing

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,14 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
 
-export default function Page() {
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/dashboard");
   return (
     <div className="formBg flex w-full justify-center items-center min-h-screen"><SignUp /></div> 
   );
 }
+export default Page;
