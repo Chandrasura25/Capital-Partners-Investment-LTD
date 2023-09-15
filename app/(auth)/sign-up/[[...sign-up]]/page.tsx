0944273efe751396c1f0754749ffd8f33d6174ca
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-15 17:10:20 +0100

    testing

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,11 +4,14 @@ import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
   const user = await currentUser();
-  if (!user) return null; // to avoid typescript warnings
   const userInfo = await fetchUser(user.id);
   if (userInfo?.onboarded) redirect("/dashboard");
-  return (
-    <div className="formBg flex w-full justify-center items-center min-h-screen"><SignUp /></div> 
-  );
+  if (!user) {
+    return (
+      <div className="formBg flex w-full justify-center items-center min-h-screen">
+        <SignUp />
+      </div>
+    );
+  }
 }
 export default Page;
