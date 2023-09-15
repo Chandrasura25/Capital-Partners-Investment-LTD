4973814f07715b5c9b8f7319396984a634f19fd9
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-15 18:04:26 +0100

    calendar

--- app/(auth)/weeks/page.tsx
+++ app/(auth)/weeks/page.tsx
@@ -1,19 +1,41 @@
-"use client"
- 
-import * as React from "react"
- 
-import { Calendar } from "@/components/ui/calendar"
+"use client";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import * as React from "react";
+
+import { Calendar } from "@/components/ui/calendar";
 
 async function Page() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border shadow"
-    />
-  )
+    <>
+      <Topbar userInfo={userInfo} />
+      <main className="flex flex-row sm:bg-[#000924]">
+        <LeftSidebar />
+        <section className="main-container relative">
+          <div className="max-w-md">
+            <div className="p-5 glassmorphism rounded-[20px] mb-8">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                className="rounded-md border shadow"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+      <Bottombar />
+    </>
+  );
 }
 
-export default Page
+export default Page;
