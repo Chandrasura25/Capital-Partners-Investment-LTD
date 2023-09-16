39c4e0b7f697ba0d50097ca76feb12c5f13644c7
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-16 10:38:51 +0100

    fix error

---
+++ components/shared/DaysCal.tsx
@@ -0,0 +1,11 @@
+"use client";
+import {useState} from 'react'
+import { Calendar } from "@/components/ui/calendar";
+
+const DaysCal = () => {
+  return (
+    <div>DaysCal</div>
+  )
+}
+
+export default DaysCal
