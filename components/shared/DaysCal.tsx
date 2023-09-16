c03c0a5314942484d70a3b68890c26ede8f3e899
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-16 10:49:19 +0100

    error fixed

--- components/shared/DaysCal.tsx
+++ components/shared/DaysCal.tsx
@@ -1,17 +1,17 @@
 "use client";
-import {useState} from 'react'
+import { useState } from "react";
 import { Calendar } from "@/components/ui/calendar";
 
 const DaysCal = () => {
   const [date, setDate] = useState<Date | undefined>(new Date());
   return (
-    <Calendar  
-    mode="single"
-    selected={date}
-    onSelect={setDate}
-    className="rounded-md border shadow"
-  />
-  )
-}
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border shadow"
+    />
+  );
+};
 
-export default DaysCal
+export default DaysCal;
