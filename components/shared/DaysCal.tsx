ce97782f120658845f46aa28a7b01b5a97982bb4
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-16 12:07:07 +0100

    style

--- components/shared/DaysCal.tsx
+++ components/shared/DaysCal.tsx
@@ -8,11 +8,6 @@ import { DateRange } from "react-day-picker";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 const today = new Date();
 const formattedDate = new Date(
   today.getFullYear(),
@@ -61,12 +56,6 @@ const DaysCal = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
           />
         </div>
       </div>
-      {/* <Popover>
-        <PopoverTrigger asChild>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-        </PopoverContent>
-      </Popover> */}
     </div>
   );
 };
