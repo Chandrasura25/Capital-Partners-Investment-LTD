69b2ae1bebf604f5e274b7c355fac1054b9152f2
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-30 13:18:24 +0100

    loader

--- app/(auth)/profile/[id]/loading.tsx
+++ app/(auth)/profile/[id]/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 const Loading = () => {
   return (
     <main className="flex-center paddings mx-auto flex-col">
-      <section className="nav-paddings w-full">
+      <section className="navPadding w-full">
         <Skeleton className="h-[274px] w-full rounded-lg bg-gray-800"  />
       </section>
       <section className="mt-6 flex w-full flex-col sm:mt-20">
