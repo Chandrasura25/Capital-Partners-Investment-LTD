8249cc88a3c34a7d6699995ae2b9b04d4dc25f49
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-19 18:15:02 +0100

    bankdetails

---
+++ lib/validations/bank.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+export const BankValidation = z.object({
+  userID: z.number().min(1),
+  username: z.string().min(3).max(30),
+  email: z.string().email().min(3).max(100),
+  accountname: z.string().min(1),
+  accountnumber: z.number().min(1),
+  bankname: z.string().min(1),
+});
