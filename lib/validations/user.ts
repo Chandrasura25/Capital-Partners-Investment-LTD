5a562e1d5d6d727d2d63c01e5ba38bbeabfd442a
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-19 08:15:38 +0100

    validation

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,16 +1,16 @@
 import * as z from "zod";
 export const UserValidation = z.object({
   profile_photo: z.string().url().nonempty(),
-  firstName: z.string().min(3).max(30),
+  firstname: z.string().min(3).max(30),
   surname: z.string().min(3).max(30),
   username: z.string().min(3).max(30),
   gender: z.string().min(3).max(20),
-  mobile_number: z.string().min(3).max(20),
+  phone: z.string().min(3).max(20),
   next_of_kin: z.string().min(3).max(30),
   mother_middle_name: z.string().min(3).max(30),
-  homeAddress: z.string().min(3).max(200),
-  officeAddress: z.string().min(3).max(200),
-  level_of_education: z.string().min(3).max(200),
+  home_address: z.string().min(3).max(200),
+  office_address: z.string().min(3).max(200),
+  education: z.string().min(3).max(200),
   email: z.string().email().min(3).max(100),
-  date_of_birth: z.string().min(3).max(200),
+  dob: z.string().min(3).max(20),
 });
