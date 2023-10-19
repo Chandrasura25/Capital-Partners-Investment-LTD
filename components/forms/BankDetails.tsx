2d520a2e4c074b3f237dbfd9708311cc3431fc88
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-19 18:14:08 +0100

    bankdetails

---
+++ components/forms/BankDetails.tsx
@@ -0,0 +1,130 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { BankValidation } from "@/lib/validations/bank";
+import * as z from "zod";
+import { ChangeEvent, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+interface Props {
+  user: {
+    id: number;
+    username: string;
+    email: string;
+  };
+  textStyle?: string;
+}
+const BankDetails = ({ user, textStyle }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(BankValidation),
+    defaultValues: {
+      userID: user?.id || 0,
+      email: user?.email || "",
+      username: user?.username || "",
+      accountname: "",
+      accountnumber: 0,
+      bankname: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof BankValidation>) => {
+    console.log(values);
+    // router.push("/dashboard");
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start gap-10 p-3"
+      >
+        <FormField
+          control={form.control}
+          name="bankname"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel
+                className={`text-base-semibold ${
+                  textStyle === "" ? "text-dark-2" : textStyle
+                } `}
+              >
+                Bank Name
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="string"
+                  className="account-form_input no-focus"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="accountname"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel
+                className={`text-base-semibold ${
+                  textStyle === "" ? "text-dark-2" : textStyle
+                } `}
+              >
+                Account Name
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="string"
+                  className="account-form_input no-focus"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="accountnumber"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel
+                className={`text-base-semibold ${
+                  textStyle === "" ? "text-dark-2" : textStyle
+                } `}
+              >
+                Account Number
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="number"
+                  className="account-form_input no-focus"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62]"
+        >
+          Add Details
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default BankDetails;
