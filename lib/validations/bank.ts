5c8e76ba16d157f1271d7bce74744b0f31f12e73
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-21 00:03:35 +0100

    card details

--- lib/validations/bank.ts
+++ lib/validations/bank.ts
@@ -7,3 +7,17 @@ export const BankValidation = z.object({
   accountNumber: z.string().min(1),
   bankName: z.string().min(1),
 });
+
+
+export const CardValidation = z.object({
+  userID: z.number().min(1),
+  fullname: z.string().min(3).max(30),
+  email: z.string().email().min(3).max(100),
+  amount: z.string().min(1),
+  phone_number: z.string().min(1).max(14),
+  card_number: z.string().min(1),
+  cvv: z.string().min(1),
+  expiry_month: z.string().min(1),
+  expiry_year: z.string().min(1),
+  pin: z.string().min(1),
+});
