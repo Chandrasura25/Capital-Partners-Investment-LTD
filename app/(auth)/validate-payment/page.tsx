b7a285db54a994877e469c75af25b8516646da97
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-21 13:27:30 +0100

    payment

---
+++ app/(auth)/validate-payment/page.tsx
@@ -0,0 +1,50 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import CardDetails from "@/components/forms/CardDetails";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+  const userDatum = await fetchUser(user.id);
+  const userInfo = userDatum?.payload;
+  if (!userInfo.onboarded) redirect("/onboarding");
+  
+//   const userData = {
+//     userID: userInfo.id,
+//     fullname: userInfo.surname + " " + userInfo.firstname,
+//     email: userInfo
+//       ? userInfo?.email
+//       : user.emailAddresses[0].emailAddress ?? "",
+//     phone_number: userInfo.phone,
+//   };
+  return (
+    <>
+      <Topbar userInfo={userInfo} />
+      <main className="flex flex-row bg-[#000924]">
+        <LeftSidebar />
+        <section className="main-container relative sm:bg-[rgba(31, 38, 135, 0.37)]">
+          <div className="w-full max-w-lg">
+            <div className="glassmorphism p-5 rounded-[20px] relative">
+              <h3 className="head-text mb-3 text-center">
+                Validate Your Payments
+              </h3>
+              {/* <p className="text-light-1 text-center mb-5">
+                Please fill your card details to continue
+              </p> */}
+              <div className="p-5">
+                {/* <CardDetails user={userData} btnTitle="Continue" textStyle="text-light-1" /> */}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Bottombar />
+    </>
+  );
+}
+
+export default Page;
