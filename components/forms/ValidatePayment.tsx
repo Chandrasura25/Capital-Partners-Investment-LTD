d6520b6dca3dba8cc9048639a108ad0dd053431c
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-21 20:01:16 +0100

    payment

--- components/forms/ValidatePayment.tsx
+++ components/forms/ValidatePayment.tsx
@@ -29,26 +29,65 @@ interface Props {
   btnTitle?: string;
 }
 const ValidatePayment = ({ user, textStyle, btnTitle }: Props) => {
-    const { toast } = useToast();
-    const router = useRouter();
-    const cardDetails = parseLocalStorageItem("cardDetails");
-    const pinInfo = parseLocalStorageItem("pin")
-    const Amount = cardDetails?.amount;
-    const stringAmount = Amount.toString();
-    const form = useForm({
-        resolver: zodResolver(PaymentValidation),
-        defaultValues: {
-          userID: user?.userID || "",
-          email: user?.email || "",
-          username: user?.username || "",
-          amount: stringAmount || "",
-          date: "",
-          flw_ref: cardDetails?.flw_ref ||  "",
-          pin: pinInfo,
-        },
-      });
-    return(
-        <></>
-    )
-}
-export default ValidatePayment
+  const { toast } = useToast();
+  const router = useRouter();
+  const cardDetails = parseLocalStorageItem("cardDetails");
+  const Amount = cardDetails?.amount;
+  const stringAmount = Amount.toString();
+  const form = useForm({
+    resolver: zodResolver(PaymentValidation),
+    defaultValues: {
+      userID: user?.userID || "",
+      email: user?.email || "",
+      username: user?.username || "",
+      amount: stringAmount || "",
+      date: "",
+      flw_ref: cardDetails?.flw_ref || "",
+      otp: "",
+    },
+  });
+   const onSubmit = async (values: z.infer<typeof PaymentValidation>) => {
+    console.log(values);
+   }
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col justify-start gap-10 p-3"
+        >
+          <FormField
+            control={form.control}
+            name="otp"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-3 w-full">
+                <FormLabel
+                  className={`text-base-semibold ${
+                    textStyle === "" ? "text-dark-2" : textStyle
+                  } `}
+                >
+                  OTP
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    type="string"
+                    className="account-form_input no-focus"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62]"
+          >
+            {btnTitle}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+export default ValidatePayment;
