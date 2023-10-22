bf74ac192506187b7149be8701931854f4d7f405
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-22 19:30:09 +0100

    index

--- app/(auth)/validate-payment/page.tsx
+++ app/(auth)/validate-payment/page.tsx
@@ -14,7 +14,7 @@ async function Page() {
   const userInfo = userDatum?.payload;
   const userStatus = userDatum?.status;
   if (!userInfo.onboarded) redirect("/onboarding");
-  const stringUserID = userInfo?.id?.toString() ?? '';
+  // const stringUserID = userInfo?.id?.toString() ?? '';
   const userData = {
     userID: userInfo?.id,
     username: userStatus ? userInfo?.username : user.username,
