453b410eab8988e782086b4afb0eac43d7c70890
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-23 17:16:37 +0100

    background

--- app/(auth)/dashboard/loading.tsx
+++ app/(auth)/dashboard/loading.tsx
@@ -3,11 +3,11 @@ import { Skeleton } from "@/components/ui/skeleton";
 const Loading = () => {
   return (
     <div className="flex flex-col h-full">
-    <Skeleton className="mb-4" height={40} />
-    <Skeleton className="mb-4" height={200} />
-    <Skeleton className="mb-4" height={200} />
-    <Skeleton className="mb-4" height={40} />
-  </div>
+      <Skeleton className="mb-4 h-40" />
+      <Skeleton className="mb-4 h-200" />
+      <Skeleton className="mb-4 h-200" />
+      <Skeleton className="mb-4 h-40" />
+    </div>
   );
 };
 
