9e61c8a4acebf2b1337790cad4db8b2ac7e1a7d6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-23 18:21:31 +0100

    spinner

--- components/forms/Withdraw.tsx
+++ components/forms/Withdraw.tsx
@@ -126,7 +126,7 @@ const Withdraw = ({ user, textStyle }: Props) => {
         />
         <Button
           type="submit"
-          className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62]"
+          className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62] flex gap-4"
         >
           Continue
           {loading && (
@@ -134,7 +134,7 @@ const Withdraw = ({ user, textStyle }: Props) => {
               <svg
                 aria-hidden="true"
                 role="status"
-                className="inline w-4 h-4 mr-3 text-white animate-spin"
+                className="inline w-5 h-5 mr-3 text-white animate-spin"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -148,7 +148,6 @@ const Withdraw = ({ user, textStyle }: Props) => {
                   fill="currentColor"
                 />
               </svg>
-              Loading...
             </div>
           )}
         </Button>
