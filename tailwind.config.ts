ed21d5f5bc6f67747c6bf6068b828110c9e3e948
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-23 16:36:27 +0100

    background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -182,6 +182,8 @@ module.exports = {
         "dark-3": "#101012",
         "dark-4": "#1F1F22",
         "light-1": "#FFFFFF",
+        "dash": "rgba(31, 38, 135, 0.37)",
+        "blue1": "#000924",
         "light-2": "#EFEFEF",
         "light-3": "#7878A3",
         "light-4": "#5C5C7B",
