35c9aabef780240b01579b44aec43b2e64b93c56
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-23 18:20:02 +0100

    spinner

--- components/forms/BankDetails.tsx
+++ components/forms/BankDetails.tsx
@@ -146,7 +146,7 @@ const BankDetails = ({ user, textStyle }: Props) => {
         />
         <Button
           type="submit"
-          className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62]"
+          className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62] flex gap-4"
         >
           Add Details
           {loading && (
@@ -154,7 +154,7 @@ const BankDetails = ({ user, textStyle }: Props) => {
               <svg
                 aria-hidden="true"
                 role="status"
-                className="inline w-4 h-4 mr-3 text-white animate-spin"
+                className="inline w-5 h-5 mr-3 text-white animate-spin"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -168,7 +168,6 @@ const BankDetails = ({ user, textStyle }: Props) => {
                   fill="currentColor"
                 />
               </svg>
-              Loading...
             </div>
           )}
         </Button>
