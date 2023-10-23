119e8bde6476d0f6912d2c9b073ae6d43425fe9b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-23 18:20:23 +0100

    spinner

--- components/forms/CardDetail.tsx
+++ components/forms/CardDetail.tsx
@@ -210,7 +210,7 @@ const CardDetail = ({ user, textStyle, btnTitle }: Props) => {
         </div>
         <Button
           type="submit"
-          className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62]"
+          className="bg-[#150B62] uppercase transition hover:bg-white flex gap-4 hover:text-[#150B62]"
         >
           {btnTitle}
           {loading && (
@@ -218,7 +218,7 @@ const CardDetail = ({ user, textStyle, btnTitle }: Props) => {
               <svg
                 aria-hidden="true"
                 role="status"
-                className="inline w-4 h-4 mr-3 text-white animate-spin"
+                className="inline w-5 h-5 mr-3 text-white animate-spin"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -232,7 +232,6 @@ const CardDetail = ({ user, textStyle, btnTitle }: Props) => {
                   fill="currentColor"
                 />
               </svg>
-              Loading...
             </div>
           )}
         </Button>
