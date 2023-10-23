40af044fd9830a084434ccf9406c565572c9bc69
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-23 18:19:22 +0100

    spinner

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -292,14 +292,14 @@ const AccountProfile = ({ user, btnTitle, textStyle }: Props) => {
             </FormItem>
           )}
         />
-        <Button type="submit" className="bg-[#43a5f6]">
+        <Button type="submit" className="bg-[#43a5f6] flex gap-4">
           {btnTitle}
           {loading && (
             <div className="text-right">
               <svg
                 aria-hidden="true"
                 role="status"
-                className="inline w-4 h-4 mr-3 text-white animate-spin"
+                className="inline w-5 h-5 mr-3 text-white animate-spin"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -313,7 +313,6 @@ const AccountProfile = ({ user, btnTitle, textStyle }: Props) => {
                   fill="currentColor"
                 />
               </svg>
-              Loading...
             </div>
           )}
         </Button>
