4e0a0bd4cbef28d88ee75c2a7278446097d99973
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-23 18:06:13 +0100

    spinner

--- components/forms/ValidatePayment.tsx
+++ components/forms/ValidatePayment.tsx
@@ -13,7 +13,8 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { PaymentValidation } from "@/lib/validations/bank";
 import * as z from "zod";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { ValidatePurchase } from "@/lib/actions/user.actions";
 import { useToast } from "@/components/ui/use-toast";
 import { ToastAction } from "@/components/ui/toast";
@@ -31,6 +32,7 @@ interface Props {
 const ValidatePayment = ({ user, textStyle, btnTitle }: Props) => {
   const { toast } = useToast();
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const cardDetails = parseLocalStorageItem("cardDetails");
   const Amount = cardDetails?.amount;
   const stringAmount = Amount.toString();
@@ -49,6 +51,7 @@ const ValidatePayment = ({ user, textStyle, btnTitle }: Props) => {
     },
   });
   const onSubmit = async (values: z.infer<typeof PaymentValidation>) => {
+    setLoading(true);
     const res = await ValidatePurchase({
       userData: {
         userID: values.userID,
@@ -61,8 +64,8 @@ const ValidatePayment = ({ user, textStyle, btnTitle }: Props) => {
       otp: values.otp,
       flw_ref: values.flw_ref,
     });
-    console.log(values, res);
     if (res.status) {
+       setLoading(false);
       toast({
         description: "Payment Validation is successful.",
         action: <ToastAction altText="Ok">Ok</ToastAction>,
@@ -71,6 +74,7 @@ const ValidatePayment = ({ user, textStyle, btnTitle }: Props) => {
       localStorage.removeItem("cardDetails");
       router.push("/dashboard");
     } else {
+       setLoading(false);
       toast({
         variant: "destructive",
         title: "Uh oh! Something went wrong.",
@@ -114,6 +118,28 @@ const ValidatePayment = ({ user, textStyle, btnTitle }: Props) => {
             className="bg-[#150B62] uppercase transition hover:bg-white hover:text-[#150B62]"
           >
             {btnTitle}
+            {loading && (
+            <div className="text-right">
+              <svg
+                aria-hidden="true"
+                role="status"
+                className="inline w-4 h-4 mr-3 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                />
+              </svg>
+              Loading...
+            </div>
+          )}
           </Button>
         </form>
       </Form>
