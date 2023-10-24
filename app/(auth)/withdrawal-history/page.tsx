7cdbecc88f052a8c8b48085a5e5a3c09f5473ba2
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-24 15:30:02 +0100

    history

--- app/(auth)/withdrawal-history/page.tsx
+++ app/(auth)/withdrawal-history/page.tsx
@@ -1,18 +1,17 @@
-import { fetchUser, fetchInvestments } from "@/lib/actions/user.actions";
+import { fetchUser, WithdrawalHistory } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
-// import BankDetails from "@/components/forms/BankDetails";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
   const userDatum = await fetchUser(user.id);
   const userInfo = userDatum?.payload;
-  const investments = await fetchInvestments(userInfo.email);
-  console.log(investments);
+  const response =await WithdrawalHistory(userInfo.id);
+  const histories = response.payload;
   if (!userInfo.onboarded) redirect("/onboarding");
   return (
     <>
@@ -22,9 +21,17 @@ async function Page() {
         <section className="main-container relative">
         <div className="max-w-lg">
             <div className="p-5 glassmorphism rounded-[20px] mb-8">
-            <h4 className="head-text uppercase mb-3">History</h4>
+            <h4 className="head-text uppercase mb-3">Withdrawal History</h4>
              <div>
-             {/* <BankDetails user={userInfo} textStyle="uppercase" /> */}
+              {histories.length > 0 ? (
+                histories.map((history: any) => (
+                  <div key={history.id}>{history.amount}</div>  
+                ))      
+              ):(
+                <p className="!text-base-regular text-white">
+                  No Withdrawal Yet
+                </p>
+              )}
              </div>
             </div>
         </div>
