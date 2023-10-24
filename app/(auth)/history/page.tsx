6af21bc234adbce78b2e8686668cf02f86697ab7
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-24 14:21:28 +0100

    history

--- app/(auth)/history/page.tsx
+++ app/(auth)/history/page.tsx
@@ -4,15 +4,15 @@ import { redirect } from "next/navigation";
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
-import Historybar from "@/components/shared/Historybar";
+import listStyles from "@/styles/List.module.css";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
   const userDatum = await fetchUser(user.id);
   const userInfo = userDatum?.payload;
-  const investments = await fetchInvestments(userInfo.email);
-  console.log(investments);
+  const investment = await fetchInvestments(userInfo.email);
+  const investments = investment.payload;
   if (!userInfo.onboarded) redirect("/onboarding");
   return (
     <>
@@ -20,14 +20,37 @@ async function Page() {
       <main className="bg-main">
         <LeftSidebar />
         <section className="main-container relative">
-        <div className="max-w-lg">
-            <div className="p-5 glassmorphism rounded-[20px] mb-8">
-            <h4 className="head-text uppercase mb-3">History</h4>
-             <div>
-             <Historybar user={userInfo} textStyle="uppercase" investments={investments} />
-             </div>
+          <div className="max-w-lg">
+            <div className={listStyles.box}>
+              <h4 className="head-text uppercase mb-3">History</h4>
+              <div className="mt-10 flex flex-col gap-5">
+                {investments.length > 0 ? (
+                  investments.map((investment: any) => (
+                    <div className="list" key={investment.id}>
+                      <div className="imgBx">
+                        <img src= "/assets/history.svg" alt="" />
+                      </div>
+                      <div className="content">
+                        <h2 className="rank">
+                          <small>unit</small>{Number(investment.amount) / 50000}
+                        </h2>
+                        <h4># {investment.amount}</h4>
+                        <hr className="w-full"></hr>
+                        <div className="flex items-center justify-between w-full">
+                        <p>{investment.createdAt}</p>
+                        <p>000</p>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <p className="!text-base-regular text-light-3">
+                    No Investment Yet
+                  </p>
+                )}
+              </div>
             </div>
-        </div>
+          </div>
         </section>
       </main>
       <Bottombar />
