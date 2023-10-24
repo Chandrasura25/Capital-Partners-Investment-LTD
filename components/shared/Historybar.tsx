58017ee83a660dec7460d74d1d3962a2196fb321
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-24 19:39:54 +0100

    withdrawal history

--- components/shared/Historybar.tsx
+++ components/shared/Historybar.tsx
@@ -3,10 +3,13 @@ import historyStyles from "@/styles/Historial.module.css";
 interface Props {
     id: number;
     amount: number;
-    date: string;
+    withdrawal_id: number;
+    bank_code: string;
+    reference:string;
     createdAt: string;
+    bank_name: string;
 }
-const Historybar = ({ id, amount, date, createdAt }: Props) => {
+const Historybar = ({ id,bank_name, amount, withdrawal_id,bank_code,reference, createdAt }: Props) => {
   const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFFF33", "#33FFFF", "#fc5f9b","#0ed095"]; // colors for the history bar
   const getRandomColors = () => {
     const shuffledColors = [...colors].sort(() => 0.5 - Math.random());
@@ -17,7 +20,17 @@ const Historybar = ({ id, amount, date, createdAt }: Props) => {
       <div
         className={historyStyles.box}
         style={{ "--clr": getRandomColors()[id % 3] }}
-      ></div>
+      >
+        <div className={historyStyles.content}>
+                <div className="icon">
+                    <img src="" alt="withdrawal-logo" className=""/>
+                </div>
+                <div className="text">
+                    <h3>{amount}</h3>
+                    <p>5 projects</p>
+                </div>
+            </div>
+      </div>
     </>
   );
 };
