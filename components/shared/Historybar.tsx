a8d9e00b6d2cdd87b68aee66670641a7e68577cb
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-24 14:22:31 +0100

    history

--- components/shared/Historybar.tsx
+++ components/shared/Historybar.tsx
@@ -1,14 +1,29 @@
-"use client";
-interface Props{
-    user:{
-
-    };
-    investments:{};
-    btnTitle? : string;
-    textStyle? : string;
+interface Props {
+  investments: {
+    amount: number;
+    date: string;
+    createdAt: string;
+  };
+  // "id": 12,
+  // "userID": 12,
+  // "username": "ade",
+  // "email": "developers@flutterwavego.com",
+  // "unitType": "type1",
+  // "dayCount": 501,
+  // "amount": 50000,
+  // "roiDaily": 150,
+  // "roiInTotal": 75000,
+  // "withdrawn": 0,
+  // "total": 125000,
+  // "balance": 200150,
+  // "date": "21/10/2023",
+  // "validTill": "NaN/NaN/NaN",
+  // "isActive": true,
+  // "createdAt": "2023-10-21T20:10:46.000Z",
+  // "updatedAt": "2023-10-22T09:01:30.000Z"
 }
-const Historybar = ({investments, user, btnTitle, textStyle }: Props) => {
-    console.log(investments)
+const Historybar = ({ investments }: Props) => {
+  console.log(investments);
   return <></>;
 };
 export default Historybar;
