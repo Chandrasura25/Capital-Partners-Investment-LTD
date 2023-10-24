9236390f90a80cc13fc41964563d73b935bdf703
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-24 15:46:14 +0100

    custom-scrollbar

--- app/(auth)/historial/page.tsx
+++ app/(auth)/historial/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
+import listStyles from "@/styles/List.module.css";
 
 async function Page() {
   const user = await currentUser();
@@ -20,9 +21,9 @@ async function Page() {
         <LeftSidebar />
         <section className="main-container relative">
         <div className="max-w-lg">
-            <div className="p-5 glassmorphism rounded-[20px] mb-5 custom-scrollbar">
+        <div className={listStyles.box}>
             <h4 className="head-text uppercase mb-3">Withdrawal History</h4>
-             <div>
+             <div className="custom-scrollbar">
               {histories.length > 0 ? (
                 histories.map((history: any) => (
                   <div key={history.id}>{history.amount}</div>  
