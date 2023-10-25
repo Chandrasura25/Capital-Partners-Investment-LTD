5b588f68243942466fb445a15f7baf4e19d48d0b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-25 15:56:26 +0100

    dashboard

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import Image from "next/image";
 import Bottombar from "@/components/shared/Bottombar";
@@ -25,7 +24,7 @@ async function Page() {
         <section className="main-container relative">
           <div className="w-full max-w-4xl">
             <div className="glassmorphism p-5 rounded-[20px] relative flex justify-between flex-wrap gap-4 md:gap-6">
-              {dashboardMenu.map((link) =>  (
+              {/* {dashboardMenu.map((link) =>  (
                   <Link
                     href={link.route}
                     key={link.label}
@@ -41,7 +40,102 @@ async function Page() {
                     />
                     <p className="mt-4 text-light-1 text-2xl md:text-5xl">{link.label}</p>
                   </Link>
-              ))}
+              ))} */}
+              <Link
+                href="#"
+                className="rounded-[20px] p-2 w-[250px] h-[200px] flex justify-center items-center flex-col gap-3 shadow-md hover:animate-in transition-all hover:scale-105"
+                style={{ background: "#ff0f5b" }}
+              >
+                <Image
+                  src="/assets/alarm.svg"
+                  alt="Days of Investment"
+                  width={80}
+                  height={80}
+                  className="invert"
+                />
+                <p className="mt-4 text-light-1 text-2xl md:text-5xl">
+                  Days of Investment
+                </p>
+              </Link>
+              <Link
+                href="#"
+                className="rounded-[20px] p-2 w-[250px] h-[200px] flex justify-center items-center flex-col gap-3 shadow-md hover:animate-in transition-all hover:scale-105"
+                style={{ background: "#be01fe" }}
+              >
+                <Image
+                  src="/assets/hourglass.svg"
+                  alt="Weeks of Investment"
+                  width={80}
+                  height={80}
+                  className="invert"
+                />
+                <p className="mt-4 text-light-1 text-2xl md:text-5xl">
+                  Weeks of Investment
+                </p>
+              </Link>
+              <Link
+                href="#"
+                className="rounded-[20px] p-2 w-[250px] h-[200px] flex justify-center items-center flex-col gap-3 shadow-md hover:animate-in transition-all hover:scale-105"
+                style={{ background: "#01b4ff" }}
+              >
+                <Image
+                  src="/assets/calendar-check.svg"
+                  alt="Months of Investment"
+                  width={80}
+                  height={80}
+                  className="invert"
+                />
+                <p className="mt-4 text-light-1 text-2xl md:text-5xl">
+                  Months of Investment
+                </p>
+              </Link>
+              <Link
+                href="#"
+                className="rounded-[20px] p-2 w-[250px] h-[200px] flex justify-center items-center flex-col gap-3 shadow-md hover:animate-in transition-all hover:scale-105"
+                style={{ background: "#ff5b0f" }}
+              >
+                <Image
+                  src="/assets/dollar-circle.svg"
+                  alt="Number of Unit"
+                  width={80}
+                  height={80}
+                  className="invert"
+                />
+                <p className="mt-4 text-light-1 text-2xl md:text-5xl">
+                  Number of Unit
+                </p>
+              </Link>
+              <Link
+                href="/add-unit"
+                className="rounded-[20px] p-2 w-[250px] h-[200px] flex justify-center items-center flex-col gap-3 shadow-md hover:animate-in transition-all hover:scale-105"
+                style={{ background: "#cd31fe" }}
+              >
+                <Image
+                  src="/assets/comments-dollar-solid.svg"
+                  alt="Additional Unit"
+                  width={80}
+                  height={80}
+                  className="invert"
+                />
+                <p className="mt-4 text-light-1 text-2xl md:text-5xl">
+                  Additional Unit
+                </p>
+              </Link>
+              <Link
+                href="/growth"
+                className="rounded-[20px] p-2 w-[250px] h-[200px] flex justify-center items-center flex-col gap-3 shadow-md hover:animate-in transition-all hover:scale-105"
+                style={{ background: "#4500ff" }}
+              >
+                <Image
+                  src="/assets/chart-646.svg"
+                  alt="Additional Unit"
+                  width={80}
+                  height={80}
+                />
+                <p className="mt-4 text-light-1 text-2xl md:text-5xl">
+                Investment Growth
+                </p>
+              </Link>
             </div>
           </div>
         </section>
