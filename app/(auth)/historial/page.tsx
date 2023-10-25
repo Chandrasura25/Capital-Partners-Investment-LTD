3ab171ea28b5ee6a98b7772cda8fc4095aa2995c
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-25 08:03:53 +0100

    clr"

--- app/(auth)/historial/page.tsx
+++ app/(auth)/historial/page.tsx
@@ -6,14 +6,14 @@ import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
 import listStyles from "@/styles/List.module.css";
 import historyStyles from "@/styles/Historial.module.css";
-import Historybar from '@/components/shared/Historybar'
+import Historybar from "@/components/shared/Historybar";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
   const userDatum = await fetchUser(user.id);
   const userInfo = userDatum?.payload;
-  const response =await WithdrawalHistory(userInfo.id);
+  const response = await WithdrawalHistory(userInfo.id);
   const histories = response.payload;
   if (!userInfo.onboarded) redirect("/onboarding");
   return (
@@ -22,22 +22,31 @@ async function Page() {
       <main className="bg-main">
         <LeftSidebar />
         <section className="main-container relative">
-        <div className="max-w-lg">
-        <div className={`${listStyles.box} bg-white custom-scrollbar`}>
-            <h4 className="head-text uppercase mb-3">Withdrawal History</h4>
-             <div className={historyStyles.container}>
-              {histories.length > 0 ? (
-                histories.map((history: any) => (
-                  <Historybar key={history.id} {...histories} /> 
-                ))      
-              ):(
-                <p className="!text-base-regular text-white">
-                  No Withdrawal Yet
-                </p>
-              )}
-             </div>  
+          <div className="max-w-lg">
+            <div className={`${listStyles.box} bg-white custom-scrollbar`}>
+              <h4 className="head-text uppercase mb-3">Withdrawal History</h4>
+              <div className={historyStyles.container}>
+                {histories.length > 0 ? (
+                  histories.map((history: any) => (
+                    <Historybar
+                      key={history.id}
+                      id={history.id}
+                      bank_name={history.bank_name}
+                      amount={history.amount}
+                      withdrawal_id={history.withdrawal_id}
+                      bank_code={history.bank_code}
+                      reference={history.reference}
+                      createdAt={history.createdAt}
+                    />
+                  ))
+                ) : (
+                  <p className="!text-base-regular text-white">
+                    No Withdrawal Yet
+                  </p>
+                )}
+              </div>
             </div>
-        </div>
+          </div>
         </section>
       </main>
       <Bottombar />
