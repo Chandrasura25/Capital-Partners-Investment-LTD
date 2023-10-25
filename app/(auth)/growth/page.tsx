fe2dcfd44f2aec04cb8a93583e9c382d63cd68ba
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-10-25 16:04:48 +0100

    growth

---
+++ app/(auth)/growth/page.tsx
@@ -0,0 +1,30 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+  const userDatum = await fetchUser(user.id);
+  const userInfo = userDatum?.payload;
+  if (!userInfo.onboarded) redirect("/onboarding");
+  return (
+    <>
+      <Topbar userInfo={userInfo} />
+      <main className="flex flex-row sm:bg-[#000924]">
+        <LeftSidebar />
+        <section className="main-container relative">
+        <div className="max-w-md">
+            <div className="p-5 glassmorphism rounded-[20px] mb-8">
+          
+            </div>
+        </div>
+        </section>
+      </main>
+      <Bottombar />
+    </>
+  );
+}
+export default Page;
