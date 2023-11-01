645ba873b19e0a91f5ff4a72f49f0c85ab96f048
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-01 12:31:56 +0100

    accordion

--- app/(root)/faqs/page.tsx
+++ app/(root)/faqs/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FaqText } from "@/constants";
-import {  fadeIn, staggerContainer } from "@/utils/motion";
+import { fadeIn, staggerContainer } from "@/utils/motion";
 import { motion } from "framer-motion";
 import {
   Accordion,
@@ -19,30 +19,18 @@ const Page = () => {
         viewport={{ once: false, amount: 0.25 }}
         className="innerWidth mx-auto flexCenter flex-col gap-5 min-h-[50vh]"
       >
-        <div>
+       <motion.div variants={fadeIn("up", "spring", 0.5, 0.75)}>
           <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="item-1">
+            {FaqText.map((faq)=>(
+            <AccordionItem value={`item-${faq.id}`} key={faq.id}>
               <AccordionTrigger>Is it accessible?</AccordionTrigger>
               <AccordionContent>
                 Yes. It adheres to the WAI-ARIA design pattern.
               </AccordionContent>
             </AccordionItem>
-            <AccordionItem value="item-2">
-              <AccordionTrigger>Is it styled?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It comes with default styles that matches the other
-                components&apos; aesthetic.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger>Is it animated?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It&apos;s animated by default, but you can disable it if
-                you prefer.
-              </AccordionContent>
-            </AccordionItem>
+            ))}
           </Accordion>
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
