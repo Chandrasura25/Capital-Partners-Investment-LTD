0fc7afb1907ed212f0b33bf2d269ae71e9712122
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-01 12:54:19 +0100

    faq

--- app/(root)/faqs/page.tsx
+++ app/(root)/faqs/page.tsx
@@ -17,14 +17,18 @@ const Page = () => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className="innerWidth mx-auto flexCenter min-h-[50vh]"
+        className="innerWidth mx-auto glex-col gap-5 flexCenter min-h-[50vh]"
       >
+        <h4>Frequently Asked Questions</h4>
         <div className="max-w-xl glassmorphism rounded p-4 h-[100vh] overflow-scroll custom-scrollbar">
           <Accordion type="single" collapsible className="w-full">
             {FaqText.map((faq, index) => (
               <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
                 <AccordionItem value={`item-${faq.id}`} key={faq.id}>
-                  <AccordionTrigger className="text-white">{faq.quest}</AccordionTrigger>
+                  <AccordionTrigger className="text-white">
+                    <span className={`text-${faq.clr} font-semibold`}>{faq.id}</span>
+                    {faq.quest}
+                    </AccordionTrigger>
                   <AccordionContent className="text-white">{faq.ans}</AccordionContent>
                 </AccordionItem>
               </motion.div>
