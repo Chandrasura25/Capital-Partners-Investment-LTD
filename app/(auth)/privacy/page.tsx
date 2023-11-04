183d6a1cdcd50907d7e078c29ef33ca228f4b9a1
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-04 13:39:14 +0100

    privacy

--- app/(auth)/privacy/page.tsx
+++ app/(auth)/privacy/page.tsx
@@ -5,6 +5,7 @@ import Topbar from "@/components/shared/Topbar";
 import { currentUser } from "@clerk/nextjs";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
+import { PrivacyText } from '@/constants';
 
 async function Page() {
   const user = await currentUser();
@@ -20,11 +21,16 @@ async function Page() {
         <section className="main-container relative">
           <div className="w-full max-w-4xl">
             <div className="glassmorphism p-5 rounded-[20px] relative flex justify-center items-center flex-col gap-4">
-              <h4 className="head-text uppercase mb-3">Privacy Policy</h4>
+              <h4 className="head-text uppercase">Privacy Policy</h4>
               <div className="p-4 text-light-1">
                 <h4 className="">
                 This Privacy Policy describes how Capital Partners Investment Limited ("Capital Partners,""we,""us," or "our") collects, uses, stores, and protects the personal information of individuals ("you" or "users") who visit our website, engage with our services, or interact with us in any other manner. We are committed to protecting your privacy and complying with applicable data protection laws. By accessing our website or using our services, you agree to the terms and conditions outlined in this Privacy Policy.
                 </h4>
+                {PrivacyText.map((text)=>(
+                  <div key={text.index}>
+                    <p className="font-bold">{text.index}. <span>{text.title}</span></p>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
