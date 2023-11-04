6846211db1e18c29881b7fa30ace2770257d12e5
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-04 13:52:57 +0100

    privacy

--- app/(auth)/privacy/page.tsx
+++ app/(auth)/privacy/page.tsx
@@ -5,7 +5,7 @@ import Topbar from "@/components/shared/Topbar";
 import { currentUser } from "@clerk/nextjs";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
-import { PrivacyText } from '@/constants';
+import { PrivacyText } from "@/constants";
 
 async function Page() {
   const user = await currentUser();
@@ -24,14 +24,29 @@ async function Page() {
               <h4 className="head-text uppercase">Privacy Policy</h4>
               <div className="p-4 text-light-1">
                 <h4 className="">
-                This Privacy Policy describes how Capital Partners Investment Limited ("Capital Partners,""we,""us," or "our") collects, uses, stores, and protects the personal information of individuals ("you" or "users") who visit our website, engage with our services, or interact with us in any other manner. We are committed to protecting your privacy and complying with applicable data protection laws. By accessing our website or using our services, you agree to the terms and conditions outlined in this Privacy Policy.
+                  This Privacy Policy describes how Capital Partners Investment
+                  Limited ("Capital Partners,""we,""us," or "our") collects,
+                  uses, stores, and protects the personal information of
+                  individuals ("you" or "users") who visit our website, engage
+                  with our services, or interact with us in any other manner. We
+                  are committed to protecting your privacy and complying with
+                  applicable data protection laws. By accessing our website or
+                  using our services, you agree to the terms and conditions
+                  outlined in this Privacy Policy.
                 </h4>
-                {PrivacyText.map((text)=>(
+                {PrivacyText.map((text) => (
                   <div key={text.index}>
-                    <p className="font-bold">{text.index}. <span>{text.title}</span></p>
-                    {text.subdetails.map((details)=>(
+                    <p className="font-bold">
+                      {text.index}. <span>{text.title}</span>
+                    </p>
+                    {text.subdetails.map((details) => (
                       <div key={details.id}>
-                        <p className="font-bold">{details.id}. <span>{details.text}</span></p>
+                        <p className="font-bold">
+                          {details.id}. <span>{details.text}</span>
+                        </p>
+                        {details.subtext != "" ? (
+                          <p>{details.subtext}</p>
+                        ) : null}
                       </div>
                     ))}
                   </div>
