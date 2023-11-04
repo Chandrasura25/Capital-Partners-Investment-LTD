7c26c7743075f1eac1665796262029503168d076
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-04 13:19:53 +0100

    privacy

--- app/(auth)/privacy/page.tsx
+++ app/(auth)/privacy/page.tsx
@@ -19,8 +19,13 @@ async function Page() {
         <LeftSidebar />
         <section className="main-container relative">
           <div className="w-full max-w-4xl">
-            <div className="glassmorphism p-5 rounded-[20px] relative flex justify-between flex-wrap gap-4 md:gap-6">
-              
+            <div className="glassmorphism p-5 rounded-[20px] relative flex justify-center items-center flex-col gap-4">
+              <h4 className="head-text uppercase mb-3">Privacy Policy</h4>
+              <div className="p-4 text-light-1">
+                <h4 className="">
+                This Privacy Policy describes how Capital Partners Investment Limited ("Capital Partners,""we,""us," or "our") collects, uses, stores, and protects the personal information of individuals ("you" or "users") who visit our website, engage with our services, or interact with us in any other manner. We are committed to protecting your privacy and complying with applicable data protection laws. By accessing our website or using our services, you agree to the terms and conditions outlined in this Privacy Policy.
+                </h4>
+              </div>
             </div>
           </div>
         </section>
