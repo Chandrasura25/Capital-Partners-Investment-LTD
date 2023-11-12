e4910ba71cd424ec4ceba8a00ac393c061e3d321
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-12 19:22:54 +0100

    admin

--- app/(admin)/admin/login/page.tsx
+++ app/(admin)/admin/login/page.tsx
@@ -1,33 +1,23 @@
-import Styles from "../styles/AdminLogin.module.css";
 
+import AdminLogin from "@/components/forms/AdminLogin";
 async function Page() {
-
   return (
     <>
       <main className="bg-main">
-      <div className="box">
-        <div className="form">
-            <h2>Sign In</h2>
-            <div className="inputBox">
-                <input type="text" required />
-                <span>Username</span>
-                <i></i>
+        <section className="main-container relative">
+          <div className="max-w-md">
+            <div className="p-5 glassmorphism rounded-[20px] mb-8">
+              <h4 className="head-text uppercase mb-3">
+                Add Your Bank Details
+              </h4>
+              <div>
+                <AdminLogin textStyle="text-light-1" />
+              </div>
             </div>
-            <div className="inputBox">
-                <input type="password" required />
-                <span>Password</span>
-                <i></i>
-            </div>
-            <div className="links">
-                <a href="#">Forgot Password?</a>
-                <a href="#">Signup</a>
-            </div>
-            <input type="submit" value="Login" />
-        </div>
-    </div>
+          </div>
+        </section>
       </main>
     </>
   );
 }
-
 export default Page;
