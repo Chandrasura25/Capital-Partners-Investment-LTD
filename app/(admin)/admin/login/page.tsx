3956f7e7cc61b620d9106ae21bacc04970cd0570
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-12 18:51:35 +0100

    admin

--- app/(admin)/admin/login/page.tsx
+++ app/(admin)/admin/login/page.tsx
@@ -1,16 +1,30 @@
+import Styles from "../styles/AdminLogin.module.css";
+
 async function Page() {
 
   return (
     <>
       <main className="bg-main">
-        <section className="main-container relative">
-          <div className="w-full max-w-3xl">
-            <div className="bg-[#150B62] p-5 rounded-[20px] relative">
-              <h3 className="head-text mb-3 text-center">admin</h3>
-            
+      <div className="box">
+        <div className="form">
+            <h2>Sign In</h2>
+            <div className="inputBox">
+                <input type="text" required />
+                <span>Username</span>
+                <i></i>
             </div>
-          </div>
-        </section>
+            <div className="inputBox">
+                <input type="password" required />
+                <span>Password</span>
+                <i></i>
+            </div>
+            <div className="links">
+                <a href="#">Forgot Password?</a>
+                <a href="#">Signup</a>
+            </div>
+            <input type="submit" value="Login" />
+        </div>
+    </div>
       </main>
     </>
   );
