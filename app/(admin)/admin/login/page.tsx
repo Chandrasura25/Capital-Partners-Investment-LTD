bc44553cd9dc06ee5d3b1bf9d387ea0b0120db6a
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-12 15:04:30 +0100

    admin

---
+++ app/(admin)/admin/login/page.tsx
@@ -0,0 +1,19 @@
+async function Page() {
+
+  return (
+    <>
+      <main className="bg-main">
+        <section className="main-container relative">
+          <div className="w-full max-w-3xl">
+            <div className="bg-[#150B62] p-5 rounded-[20px] relative">
+              <h3 className="head-text mb-3 text-center">admin</h3>
+            
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Page;
