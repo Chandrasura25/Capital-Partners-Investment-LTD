a3457adf6058457118f6b7832a00346d123243a1
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-12 19:24:14 +0100

    Admin

--- app/(admin)/admin/login/page.tsx
+++ app/(admin)/admin/login/page.tsx
@@ -8,7 +8,7 @@ async function Page() {
           <div className="max-w-md">
             <div className="p-5 glassmorphism rounded-[20px] mb-8">
               <h4 className="head-text uppercase mb-3">
-                Add Your Bank Details
+                Admin Signup
               </h4>
               <div>
                 <AdminLogin textStyle="text-light-1" />
