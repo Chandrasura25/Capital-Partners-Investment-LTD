4cc5dbd18bb026871a709d0c2a5cd23ecbe4c6a5
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-14 19:22:17 +0100

    admin dashboard

--- app/(admin)/admin/login/page.tsx
+++ app/(admin)/admin/login/page.tsx
@@ -8,8 +8,8 @@ async function Page() {
       <section className="main-container relative">
           <div className={styles.box}>
             <div className={styles.form}>
-              <h3 className="head-text mb-3 uppercase text-center">Admin Login</h3>
-              <div className="p-4 flex justify-center">
+              <h3 className="head-text uppercase text-center">Admin Login</h3>
+              <div className="flex justify-center">
                 <AdminLogin textStyle="text-light-1" />
               </div>
             </div>
