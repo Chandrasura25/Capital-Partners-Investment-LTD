4b16e6d803cd42cc970f733800b3a7629af7b837
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 14:46:26 +0100

    right sidebar

--- components/forms/AdminLogin.tsx
+++ components/forms/AdminLogin.tsx
@@ -130,7 +130,7 @@ const AdminLogin = ({ textStyle }: Props) => {
               <svg
                 aria-hidden="true"
                 role="status"
-                className="inline w-5 h-5 mr-3 text-[#150B62] animate-spin"
+                className="inline w-5 h-5 mr-3 text-white animate-spin"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
