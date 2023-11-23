2f6ea59bcc3221164ecbfc01bfb084c15a150dd0
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 03:23:42 +0100

    admin

--- app/(admin)/admindashboard/page.tsx
+++ app/(admin)/admindashboard/page.tsx
@@ -1,13 +1,17 @@
 
 import Topbar from "@/components/admins/Topbar";
-import Storage from "@/components/admins/Storage";
 import Bottombar from "@/components/admins/Bottombar";
+import LeftSidebar from "@/components/admins/LeftSidebar";
+import { parseLocalStorageItem } from "@/lib/utils";
+import { redirect } from "next/navigation";
 async function Page() {
+  const adminDetails = parseLocalStorageItem("admin");
+  if(!adminDetails.email) redirect('/');
   return (
     <>
-    <Storage />
      <Topbar />
       <main className="bg-main">
+      <LeftSidebar />
         <section className="main-container relative"></section>
       </main>
       <Bottombar/>
