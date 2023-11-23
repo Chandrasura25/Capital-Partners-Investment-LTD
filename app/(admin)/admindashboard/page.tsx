bfff7d3118c2bfae819549058087d9b2b873e4f5
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 15:36:12 +0100

    check

--- app/(admin)/admindashboard/page.tsx
+++ app/(admin)/admindashboard/page.tsx
@@ -6,7 +6,7 @@ import { parseLocalStorageItem } from "@/lib/utils";
 import { redirect } from "next/navigation";
 async function Page() {
   const adminDetails = parseLocalStorageItem("admin")
-  if (!adminDetails || !adminDetails?.email) {
+  if (adminDetails === undefined) {
     redirect("/");
   }
   return (
