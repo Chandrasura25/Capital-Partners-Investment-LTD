395bb76f5e82a7ae4fcc67b3afdaa56abec692ab
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 16:12:18 +0100

    send broadcast

--- lib/validations/admin.ts
+++ lib/validations/admin.ts
@@ -3,3 +3,13 @@ export const AdminValidation = z.object({
   email: z.string().email().min(3).max(100),
   password: z.string().min(3).max(20),
 });
+export const BroadcastValidation = z.object({
+  headings:z.string().min(3),
+  greetings: z.string().min(5),
+  message: z.string().min(5),
+  image: z.string().min(3),
+  imageDescription: z.string().min(3),
+  sender: z.string().min(3),
+  position: z.string().min(3),
+  subject: z.string().min(3)
+});
