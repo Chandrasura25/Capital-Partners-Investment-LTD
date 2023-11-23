fe45f1b6aa5563a80b924501635e15ffd30103be
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 14:01:12 +0100

    local storage

--- components/admins/Storage.tsx
+++ components/admins/Storage.tsx
@@ -3,7 +3,8 @@ import { parseLocalStorageItem } from "@/lib/utils";
 import { redirect } from "next/navigation";
 function Storage(){
   const adminDetails = parseLocalStorageItem("admin");
-  if (!adminDetails?.status) redirect("/");
+  console.log(adminDetails)
+  // if (!adminDetails?.status) redirect("/");
    return <></>
 }
 export default Storage;
