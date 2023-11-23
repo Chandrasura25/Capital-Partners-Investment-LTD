c46cbca7cac14004df1fe370acce592e074dba7d
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 14:52:21 +0100

    right sidebar

--- app/(admin)/admindashboard/page.tsx
+++ app/(admin)/admindashboard/page.tsx
@@ -1,20 +1,21 @@
-
 import Topbar from "@/components/admins/Topbar";
 import Bottombar from "@/components/admins/Bottombar";
 import LeftSidebar from "@/components/admins/LeftSidebar";
 import RightSidebar from "@/components/admins/RightSidebar";
-import Storage from "@/components/admins/Storage"; 
+import Storage from "@/components/admins/Storage";
 async function Page() {
   return (
     <>
-     <Storage/>
-     <Topbar />
+      <Storage />
+      <Topbar />
       <main className="bg-main">
-      <LeftSidebar />
-        <section className="main-container relative"></section>
+        <LeftSidebar />
+        <section className="main-container">
+          <div className="w-full max-w-4xl"></div>
+        </section>
         <RightSidebar />
       </main>
-      <Bottombar/>
+      <Bottombar />
     </>
   );
 }
