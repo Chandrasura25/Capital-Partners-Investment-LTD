9a51416aad858b68c82ac0cf1cdd3287b6f88993
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 02:46:26 +0100

    admin

--- app/(admin)/admindashboard/page.tsx
+++ app/(admin)/admindashboard/page.tsx
@@ -1,12 +1,16 @@
 import { parseLocalStorageItem } from "@/lib/utils";
+import Topbar from "@/components/admins/Topbar";
+import Bottombar from "@/components/admins/Bottombar";
 async function Page() {
   const adminDetails = parseLocalStorageItem("admin");
   console.log(adminDetails);
   return (
     <>
+     <Topbar />
       <main className="bg-main">
         <section className="main-container relative"></section>
       </main>
+      <Bottombar/>
     </>
   );
 }
