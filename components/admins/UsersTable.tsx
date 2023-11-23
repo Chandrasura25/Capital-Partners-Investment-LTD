0d7e2f7aefc649492f290ec8a6da21522408ad09
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 19:23:05 +0100

    all users

--- components/admins/UsersTable.tsx
+++ components/admins/UsersTable.tsx
@@ -2,43 +2,56 @@
 import Image from "next/image";
 import Link from "next/link";
 import {
-    ColumnDef,
-    ColumnFiltersState,
-    SortingState,
-    VisibilityState,
-    flexRender,
-    getCoreRowModel,
-    getFilteredRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-    useReactTable,
-  } from "@tanstack/react-table"
-  import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
- 
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-  import { Input } from "@/components/ui/input"
-  import { Checkbox } from "@/components/ui/checkbox"
-  import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function UsersTable() {
-  return (
-    <></>
-  );
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://cap-partners-investment.cyclic.app/api/v0/admin/adminapis/get_all_users"
+      )
+      .then((res) => {
+        setUsers(res.data.payload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return <></>;
 }
 
 export default UsersTable;
