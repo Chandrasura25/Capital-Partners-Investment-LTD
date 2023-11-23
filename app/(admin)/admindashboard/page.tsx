f6889ed73663066b681c930d20f6c1bb81621a56
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 19:15:06 +0100

    checkbox and dropdown

--- app/(admin)/admindashboard/page.tsx
+++ app/(admin)/admindashboard/page.tsx
@@ -2,10 +2,11 @@ import Topbar from "@/components/admins/Topbar";
 import Bottombar from "@/components/admins/Bottombar";
 import LeftSidebar from "@/components/admins/LeftSidebar";
 import RightSidebar from "@/components/admins/RightSidebar";
+import UsersTable from "@/components/admins/UsersTable";
 import { parseLocalStorageItem } from "@/lib/utils";
 import { redirect } from "next/navigation";
 async function Page() {
-  const adminDetails = parseLocalStorageItem("admin")
+  const adminDetails = parseLocalStorageItem("admin");
   if (adminDetails === undefined) {
     redirect("/");
   }
@@ -15,7 +16,9 @@ async function Page() {
       <main className="bg-main">
         <LeftSidebar />
         <section className="main-container">
-          <div className="w-full max-w-4xl"></div>
+          <div className="w-full max-w-4xl">
+            <UsersTable />
+          </div>
         </section>
         <RightSidebar />
       </main>
--- components/admins/UsersTable.tsx
+++ components/admins/UsersTable.tsx
@@ -1,6 +1,40 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    SortingState,
+    VisibilityState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+  } from "@tanstack/react-table"
+  import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+ 
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  import { Input } from "@/components/ui/input"
+  import { Checkbox } from "@/components/ui/checkbox"
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
 function UsersTable() {
   return (
     <></>
