d8a94ba441116700b2cee14ef3f6d839db567efa
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 03:24:41 +0100

    admin

--- components/admins/Storage.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { parseLocalStorageItem } from "@/lib/utils";
-function Storage(){
-  const adminDetails = parseLocalStorageItem("admin");
-  console.log(adminDetails);
-   return <></>
-}
-export default Storage;
