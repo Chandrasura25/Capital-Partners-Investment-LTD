e1f0851cdf30edebb1e28b2f967023c2ef7d3ae5
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 16:12:52 +0100

    send broadcast

--- components/admins/Storage.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { parseLocalStorageItem } from "@/lib/utils";
-import { redirect } from "next/navigation";
-function Storage(){
-  const adminDetails = parseLocalStorageItem("admin");
-  if (!adminDetails || !adminDetails?.email) redirect("/");
-   return <></>
-}
-export default Storage;
