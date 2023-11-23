fb032648f5f2b13863b8bff2e18f538b5dcf4270
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-11-23 16:13:32 +0100

    send broadcast

--- components/admins/RightSidebar.tsx
+++ components/admins/RightSidebar.tsx
@@ -3,11 +3,11 @@ async function RightSidebar() {
   return (
     <section className='custom-scrollbar rightsidebar'>
       <div className='flex flex-1 flex-col justify-start'>
-        <h3 className='text-heading4-medium text-light-1'>
+        <h3 className='text-heading4-medium text-light-1 uppercase'>
           Send A Broadcast Message
         </h3>
-        <div className='mt-4 flex w-[350px] flex-col gap-9'>
-            <SendBroadcast/>
+        <div className='mt-4 flex w-[350px]'>
+            <SendBroadcast textStyle="uppercase"/>
         </div>
       </div>
 
